1db1b0173f1236bb9f7fa5abf4ea3aafdfcc513f
Gabriel Cayetano <gcayetanonovalio@gmail.com>
2018-03-29 13:27:28 +0200

    Update to ES6 class syntax

--- src/index.js
+++ src/index.js
@@ -1,54 +1,57 @@
-const electron = require ('electron');
-const path = require ('path');
-const fs = require ('fs');
-const jsonfile = require ('jsonfile');
+const electron = require('electron');
+const path = require('path');
+const fs = require('fs');
+const jsonfile = require('jsonfile');
 
 const app = electron.app || electron.remote.app;
-const userData = app.getPath ('userData');
+const userData = app.getPath('userData');
 
-const Storage = (options = null) => {
-  const defaultOptions = {
-    filename: 'config.json'
-  };
+class Storage {
+  constructor(options = null) {
+    this.options = null;
+    this.data = null;
 
-  if (options) {
-    this.options = options;
-  } else {
-    this.options = defaultOptions;
+    // Default options
+    const defaultOptions = {
+      filename: 'config.json'
+    };
+
+    if (options) {
+      this.options = options;
+    } else {
+      this.options = defaultOptions;
+    }
+
+    // Set filepath
+    this.options.filepath = path.join(userData, this.options.filename);
+    console.log(path.join(userData, this.options.filename));
+
+    // Create file if not exists or load data if it is
+    fs.exists(this.options.filepath, found => {
+      if (!found) {
+        this.data = {};
+        jsonfile.writeFile(this.options.filepath, {}, err => {
+          console.log(err);
+        });
+      } else {
+        jsonfile.readFile(this.options.filepath,(err, obj) => {
+          if (err) console.log(err);
+
+          if (obj) {
+            this.data = obj;
+          }
+        });
+      }
+    });
   }
 
-  // Set filepath
-  this.options.filepath = path.join (userData, this.options.filename);
-  console.log (path.join (userData, this.options.filename));
-
-  // Create file if not exists or load data if it is
-  fs.exists (this.options.filepath, found => {
-    if (!found) {
-      this.data = {};
-      jsonfile.writeFile (this.options.filepath, {}, err => {
-        console.log (err);
-      });
-    } else {
-      jsonfile.readFile (this.options.filepath, (err, obj) => {
-        if (err) console.log (err);
-
-        if (obj) {
-          this.data = obj;
-        }
-      });
-    }
-  });
-};
-
-/**
- * Get data by key from storage
- * @param {string} key
- */
-const get = key => {
-
-};
-
-
-Storage.prototype.get = get;
+  /**
+   * Get data by key from storage
+   * @param {string} key
+   */
+  get(key) {
+    
+  }
+}
 
 module.exports = Storage;
